3855a71b80825e1ceaec2c5682097c2d123ef398
13klove <13klove@naver.com>
2023-11-19 16:05:49 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/dddsample/DddSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dddsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DddSampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<DddSampleApplication>(*args)
+}

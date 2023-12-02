0ebb957ef50fbeaa99c6cff96d01e4f2c70e1672
13klove <13klove@naver.com>
2023-12-02 10:48:32 +0900

    product setting

--- src/main/kotlin/com/example/dddsample/product/domain/Product.kt
+++ src/main/kotlin/com/example/dddsample/product/domain/Product.kt
@@ -11,27 +11,57 @@ class Product(
     price: Long,
     stock: Int,
     status: ProductStatus,
-    categoryIds: Set<Long>
+    categoryIds: MutableList<Long>,
+    productFiles: MutableList<ProductFile>
 ) {
 
     @Id
-    @GeneratedValue
-    val id: Long = 0
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: ProductId = 0
 
     var name: String = name
+        private set
     var description: String = description
+        private set
     var price: Long = price
+        private set
     var stock: Int = stock
+        private set
     @Enumerated(EnumType.STRING)
     var status: ProductStatus = status
+        private set
 
     @ElementCollection
     @CollectionTable(name = "product_category_mapping", joinColumns = [JoinColumn(name = "product_id")])
-    var categoryIds: Set<Long> = categoryIds
+    @Column(name = "category_id")
+    var categoryIds: MutableList<Long> = categoryIds
+        private set
 
+    @ElementCollection
+    @CollectionTable(name = "product_file", joinColumns = [JoinColumn(name = "product_id")])
+    var productFiles: MutableList<ProductFile> = productFiles
+        private set
 
     val createdAt: Instant = Instant.now()
     var updatedAt: Instant = Instant.now()
+        private set
 
+    fun updateName(name: String) {
+        this.name = name
+    }
+
+    fun updateCategoryIds(categoryIds: MutableList<Long>) {
+        this.categoryIds = categoryIds
+    }
+
+    fun addFile(file: ProductFile) {
+        this.productFiles.add(file)
+    }
+
+    fun removeFile(fileId: ProductFileId) {
+        this.productFiles.removeIf { it.id == fileId }
+    }
 
 }
+
+typealias ProductId = Long
--- src/main/kotlin/com/example/dddsample/product/infra/ProductRepository.kt
+++ src/main/kotlin/com/example/dddsample/product/infra/ProductRepository.kt
@@ -1,4 +1,8 @@
 package com.example.dddsample.product.infra
 
-interface ProductRepository {
+import com.example.dddsample.product.domain.Product
+import com.example.dddsample.product.domain.ProductId
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface ProductRepository : JpaRepository<Product, ProductId> {
 }
